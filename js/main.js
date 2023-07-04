ceb5ff590308c38bd9ecf32df7e9ab94d67ffcf8
alex-main-academy <oleksandrilnytskyi@gmail.com>
2023-07-04 12:44:02 +0300

    change-file-structure

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-const inputFileElement = document.querySelector(".js-input-file");
-const formBlockElement = document.querySelector(".js-form-block");
-const inputFileBlock = document.createElement("div");
-const inputFileName = document.createElement("p");
-const inputFileCloseIcon = document.createElement("img");
-inputFileName.classList.add("form__file_name");
-inputFileBlock.classList.add("form__file_block");
-inputFileCloseIcon.classList.add("form__file_icon");
-inputFileCloseIcon.setAttribute("src", "img/icons/close.svg");
-
-inputFileBlock.append(inputFileCloseIcon, inputFileName);
-
-const addInputFileName = () => {
-  inputFileElement.addEventListener("change", (event) => {
-    inputFileName.innerText = event.target.value.split("\\").pop();
-    formBlockElement.append(inputFileBlock);
-
-    inputFileCloseIcon.addEventListener("click", () => {
-      inputFileElement.value = null;
-      inputFileName.innerText = inputFileElement.value;
-      inputFileBlock.remove(inputFileCloseIcon);
-    });
-  });
-};
-
-addInputFileName();
-
 // scroll top
 const scrollButtonElement = document.querySelector(".js-scroll-top");
 scrollButtonElement.addEventListener("click", () => {
@@ -52,3 +25,56 @@ window.addEventListener("scroll", function () {
     scrollButtonElement.style.opacity = "0";
   }
 });
+
+// menu
+const menuElement = document.querySelector(".js-menu");
+const openMenuButton = document.querySelector(".js-open-menu");
+const closeMenuButton = document.querySelector(".js-close-menu");
+const handleOpenMenu = () => {
+  document.body.style.cssText = `
+        overflow: hidden;
+    `;
+  menuElement.style.cssText = `
+        opacity: 1;
+        transform: translate(0);
+    `;
+};
+const handleCloseMenu = () => {
+  document.body.style.cssText = `
+        overflow: auto;
+    `;
+  menuElement.style.cssText = `
+        opacity: 0;
+        transform: translate(-100%);
+    `;
+};
+openMenuButton.addEventListener("click", handleOpenMenu);
+closeMenuButton.addEventListener("click", handleCloseMenu);
+
+// file name
+const inputFileElement = document.querySelector(".js-input-file");
+const formBlockElement = document.querySelector(".js-form-block");
+const inputFileBlock = document.createElement("div");
+const inputFileName = document.createElement("p");
+const inputFileCloseIcon = document.createElement("img");
+inputFileName.classList.add("form__file_name");
+inputFileBlock.classList.add("form__file_block");
+inputFileCloseIcon.classList.add("form__file_icon");
+inputFileCloseIcon.setAttribute("src", "img/icons/close.svg");
+
+inputFileBlock.append(inputFileCloseIcon, inputFileName);
+
+const addInputFileName = () => {
+  inputFileElement?.addEventListener("change", (event) => {
+    inputFileName.innerText = event.target.value.split("\\").pop();
+    formBlockElement.append(inputFileBlock);
+
+    inputFileCloseIcon.addEventListener("click", () => {
+      inputFileElement.value = null;
+      inputFileName.innerText = inputFileElement.value;
+      inputFileBlock.remove(inputFileCloseIcon);
+    });
+  });
+};
+
+addInputFileName();
